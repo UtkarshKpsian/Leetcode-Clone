ffa7084a966687f7a77b0b64242348ac208dc3ce
Utkarsh Mishra <mishrautkarsh319@gmail.com>
2025-07-29 18:57:17 +0530

    Fix CORS to allow Netlify frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,22 @@ const cors = require('cors')
 
 // console.log("Hello")
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://leetcode-clone-utkarsh.netlify.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true 
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
